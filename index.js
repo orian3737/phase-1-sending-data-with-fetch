5ec70a5b577e823e7d8ea6aa4b46508c6e6089bb
Orian shillz <91493524+orian3737@users.noreply.github.com>
2024-04-05 12:12:03 -0400

    3 passing tests

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+
+
+function submitData(name, email) {
+    const url = 'http://localhost:3000/users'; // Define url variable here
+    const data = {
+      name: name,
+      email: email
+    };
+  
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      const id = data.id;
+      document.body.append(id);
+    })
+    .catch(error => {
+      document.body.append(error.message);
+    });
+  }
